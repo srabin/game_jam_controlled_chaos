816d1cfcacf8bb5950e800a286d2092ba66ebfbd
srabin <sethmr96@gmail.com>
2025-04-13 10:24:46 -0400

    make movement analogue

--- player.gd
+++ player.gd
@@ -2,19 +2,17 @@ extends CharacterBody2D
 
 @onready var dash_length_timer = $DashLengthTimer
 var is_dashing = false
+var move_input = Vector2.ZERO
 
-const SPEED = 800.0
+const SPEED = 400.0
 const DASH_SPEED = SPEED * 4
-const DECELERATION_RATE = SPEED * 0.02
+const DECELERATION_RATE = SPEED * 0.03
 const ACCELLERATION_RATE = SPEED * 0.18
+
 
 
 func _on_dash_length_timer_timeout() -> void:
 	is_dashing = false
-	#velocity.x = move_toward(velocity.x, 0, DECELERATION_RATE)
-	#velocity.y = move_toward(velocity.y, 0, DECELERATION_RATE)
-	#velocity.x = 0
-	#velocity.y = 0
 	
 func _process(delta: float) -> void:
 	var direction = Input.get_vector("look_left", "look_right", "look_up", "look_down")
@@ -22,19 +20,14 @@ func _process(delta: float) -> void:
 	look_at(player_position + direction)
 	
 func _physics_process(delta: float) -> void:
-	var horizontal := Input.get_axis("move_left", "move_right")
-	var vertical := Input.get_axis("move_up", "move_down")
-	var dash := Input.is_action_just_pressed("dash")
+	var horizontal := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var vertical := Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	var dash := Input.get_action_strength("dash")
 		
 	velocity.x = move_toward(velocity.x, horizontal * SPEED, ACCELLERATION_RATE)
 	velocity.y = move_toward(velocity.y, vertical * SPEED, ACCELLERATION_RATE)
 		
 	if dash and not is_dashing:
-		#dash_length_timer.start()
-		#is_dashing = true
-		#var click_position = get_global_mouse_position()
-		#var target_position = (click_position - position).normalized()
-		#velocity = target_position * DASH_SPEED
 		velocity.x = horizontal * DASH_SPEED
 		velocity.y = vertical * DASH_SPEED
 
