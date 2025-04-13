c6ff56e8229284400117477a4903c381f9a0fa21
mh859 <mh859@cornell.edu>
2025-04-13 13:21:50 -0400

    camera zoom and track

--- environment/platform.gd
+++ environment/platform.gd
@@ -13,6 +13,8 @@ func _process(delta: float) -> void:
 
 func _on_timer_timeout():
 	#Can Add an if to skip this once minimum platform size is reached
+	# also can add a counter like timouts_till_start that'll allow us to have different behaviors at different stages of game
+	#ex: on first time out say "things are changing", on second timout "it's coming!", third timeout "things will start falling in 3.. 2... 1 .." etc. 
 	var rect = self.get_used_rect()
 	var new_rect = rect.grow(-2)
 	print(rect)
