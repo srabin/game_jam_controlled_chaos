9f65ded4bb975fea8d1435963fdb247acdb7a794
srabin <sethmr96@gmail.com>
2025-04-15 21:47:27 -0400

    refactor names and include start_idle function

--- player.gd
+++ player.gd
@@ -40,31 +40,34 @@ func _process(delta: float) -> void:
 	# This needs to be rotated 90 degrees because look_at looks to the right of the sprite by default
 	look_at(player_position + direction.rotated(-1*(PI / 2)))
 
-	
-func _dash(horizontal, vertical, dash, delta):	
-	state = States.DASHING
-	animation_player.play("dash", -1, dash_speed)
-	animation_player.animation_finished.connect(func(_animation): has_dashed = true)
-	velocity.x = horizontal * DASH_SPEED
-	velocity.y = vertical * DASH_SPEED
-
-
-func _move(horizontal, vertical, delta):
+func _start_move(horizontal, vertical, delta):
 	state = States.MOVING
 	if not animation_player.current_animation == "walk" or not animation_player.is_playing():
 		animation_player.play("walk")
 	velocity.x = move_toward(velocity.x, horizontal * SPEED, ACCELLERATION_RATE)
 	velocity.y = move_toward(velocity.y, vertical * SPEED, ACCELLERATION_RATE)
 
-func _light_attack():
+func _start_block():
+	state = States.BLOCKING
+	animation_player.play("block", -1, block_speed)
+	animation_player.animation_finished.connect(func(_animation): has_blocked = true)
+	
+func _start_dash(horizontal, vertical, dash, delta):	
+	state = States.DASHING
+	animation_player.play("dash", -1, dash_speed)
+	animation_player.animation_finished.connect(func(_animation): has_dashed = true)
+	velocity.x = horizontal * DASH_SPEED
+	velocity.y = vertical * DASH_SPEED
+
+func _start_light_attack():
 	state = States.ATTACKING
 	animation_player.play("light_attack", -1, attack_speed)
 	animation_player.animation_finished.connect(func(_animation): has_attacked = true)
 	
-func _block():
-	state = States.BLOCKING
-	animation_player.play("block", -1, block_speed)
-	animation_player.animation_finished.connect(func(_animation): has_blocked = true)
+func _start_idle():
+	state = States.IDLE
+	if not animation_player.current_animation == "idle" or not animation_player.is_playing():
+		animation_player.play("idle")
 	
 func _physics_process(delta: float) -> void:
 	var horizontal := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -75,47 +78,46 @@ func _physics_process(delta: float) -> void:
 
 	match state:
 		States.IDLE:
-			animation_player.play("idle")
 			if horizontal or vertical:
-				self._move(horizontal, vertical, delta)
+				self._start_move(horizontal, vertical, delta)
 			if dash:
-				self._dash(horizontal, vertical, dash, delta)
+				self._start_dash(horizontal, vertical, dash, delta)
 			if light_attack:
-				self._light_attack()
+				self._start_light_attack()
 			if block:
-				self._block()
-			
-		States.DASHING:
-			if has_dashed:
-				state = States.IDLE
-				has_dashed = false
-			if light_attack:
-				self._light_attack()
-			if block:
-				self._block()
+				self._start_block()
 		States.MOVING:
 			if horizontal or vertical:
-				self._move(horizontal, vertical, delta)
+				self._start_move(horizontal, vertical, delta)
 			else:
-				state = States.IDLE
+				self._start_idle()
 			if dash:
-				self._dash(horizontal, vertical, dash, delta)
+				self._start_dash(horizontal, vertical, dash, delta)
 			if light_attack:
-				self._light_attack()
+				self._start_light_attack()
 			if block:
-				self._block()
+				self._start_block()
+		States.BLOCKING:
+			if has_blocked:
+				self._start_idle()
+				has_blocked = false
+		States.DASHING:
+			if has_dashed:
+				self._start_idle()
+				has_dashed = false
+			if light_attack:
+				self._start_light_attack()
+			if block:
+				self._start_block()
+		States.ATTACKING:
+			if has_attacked:
+				self._start_idle()
+				has_attacked = false
 		States.HURTING:
 			pass
 		States.FALLING:
 			pass
-		States.ATTACKING:
-			if has_attacked:
-				state = States.IDLE
-				has_attacked = false
-		States.BLOCKING:
-			if has_blocked:
-				state = States.IDLE
-				has_blocked = false
+
 	
 	velocity.x = move_toward(velocity.x, 0, DECELERATION_RATE)
 	velocity.y = move_toward(velocity.y, 0, DECELERATION_RATE)
