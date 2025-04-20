6813c762c1c92d5ecdb27f1a438cd80bf2a580ec
mh859 <mh859@cornell.edu>
2025-04-19 23:00:01 -0400

    percentage display

---
+++ percentage_display.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+@onready var player_one: Label = $MarginContainer/HBoxContainer/VBoxContainer2/PlayerOne
+@onready var player_two: Label = $MarginContainer/HBoxContainer/VBoxContainer/PlayerTwo
+
+@onready var tramates: CharacterBody2D = $"../Tramates"
+@onready var mentaria: CharacterBody2D = $"../Mentaria"
+
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var tramates_percentage = str(tramates.percentage) + "%"
+	var mentaria_percentage = str(mentaria.percentage) + "%"
+	player_one.text = tramates_percentage
+	player_two.text = mentaria_percentage
