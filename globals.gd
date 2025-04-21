36e969f3403e81fb196992b35f95d9b851b18cb7
srabin <sethmr96@gmail.com>
2025-04-20 21:24:22 -0400

    add sfx and music bus

--- globals.gd
+++ globals.gd
@@ -19,18 +19,21 @@ func _ready() -> void:
 	select_beep.set_stream(select_beep_stream)
 	select_beep.pitch_scale = 0.75
 	select_beep.name = "SelectBeep"
+	select_beep.bus = "SFX"
 	add_child(select_beep)
 	
 	select_click = AudioStreamPlayer.new()
 	var select_click_stream = load("res://sounds/select_click.wav")
 	select_click.set_stream(select_click_stream)
 	select_click.name = "SelectClick"
+	select_click.bus = "SFX"
 	add_child(select_click)
 	
 	title_track = AudioStreamPlayer.new()
 	var title_track_stream = load("res://sounds/title_track.wav")
 	title_track.set_stream(title_track_stream)
 	title_track.name = "TitleTrack"
+	title_track.bus = "Music"
 	title_track.finished.connect(_on_title_track_finished)
 	add_child(title_track)
 	
@@ -38,6 +41,7 @@ func _ready() -> void:
 	var fight_track_stream = load("res://sounds/fight_track.wav")
 	fight_track.set_stream(fight_track_stream)
 	fight_track.name = "FightTrack"
+	fight_track.bus = "Music"
 	fight_track.finished.connect(_on_fight_track_finished)
 	add_child(fight_track)
 	
