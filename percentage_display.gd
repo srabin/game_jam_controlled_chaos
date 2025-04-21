ad8165f2b733bde3e34eec2698caab7244ec8613
srabin <sethmr96@gmail.com>
2025-04-21 15:21:01 -0400

    update percentage display to fix ui bug, fix music mix a bit

--- percentage_display.gd
+++ percentage_display.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
-@onready var player_one: Label = $MarginContainer/HBoxContainer/VBoxContainer2/PlayerOne
-@onready var player_two: Label = $MarginContainer/HBoxContainer/VBoxContainer/PlayerTwo
+@onready var player_one: Label = $MarginContainer/HBoxContainer/VBoxContainer2/HBoxContainer/PlayerOne
+@onready var player_two: Label = $MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/PlayerTwo
+
 
 @onready var tramates: CharacterBody2D = $"../Tramates"
 @onready var mentaria: CharacterBody2D = $"../Mentaria"
@@ -11,7 +12,7 @@ extends CanvasLayer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var tramates_percentage = str(tramates.percentage) + "%"
-	var mentaria_percentage = str(mentaria.percentage) + "%"
+	var tramates_percentage = str(tramates.percentage)
+	var mentaria_percentage = str(mentaria.percentage)
 	player_one.text = tramates_percentage
 	player_two.text = mentaria_percentage
